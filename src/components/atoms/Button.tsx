bb589cc527e1c4b36c404f67ba28f424ef5c4d4f
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2023-12-10 17:28:02 -0300

    adding animation to contact button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,7 +1,7 @@
 const Button: React.FC<{ children: React.ReactNode }> = (props) => {
     //passar href como props pois o botão vai ser usado várias vezes
     return (
-        <a className="text-white tracking-wide w-fit border-b-2 border-white" href="mailto:giovannadefreitasaraujo@hotmail.com">{props.children}</a>
+        <a className="text-white tracking-wide w-fit border-b-2 border-white hover:text-pink-300 hover:border-pink-300 duration-500 hover:animate-pulse" href="mailto:giovannadefreitasaraujo@hotmail.com">{props.children}</a>
     )
 }
 
