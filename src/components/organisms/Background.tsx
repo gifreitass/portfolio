8e8b629843bbc603ae44f650e27a252b24c341df
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2024-02-14 16:53:36 -0300

    changing video settings

--- src/components/organisms/Background.tsx
+++ src/components/organisms/Background.tsx
@@ -1,6 +1,6 @@
 const Background: React.FC = () => {
     return (
-        <video className="absolute w-screen h-full object-cover" data-wf-ignore src="videos/sea.mp4" autoPlay loop muted playsInline preload="auto" onContextMenu={() => false} />
+        <video className="absolute w-screen h-full object-cover" src="videos/sea.mp4" autoPlay loop muted playsInline preload="auto" onContextMenu={() => false} />
     )
 }
 
