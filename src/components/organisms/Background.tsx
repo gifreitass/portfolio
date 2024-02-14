2a88a190c123d6b8d6a3d2f758ac4d87fe85f605
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2024-02-14 15:53:23 -0300

    changing video settings

--- src/components/organisms/Background.tsx
+++ src/components/organisms/Background.tsx
@@ -1,12 +1,8 @@
-import useMobile from "@/hooks/useMobile";
-
 const Background: React.FC = () => {
-    const isMobile = useMobile()
-
-    const videoSource = isMobile ? "videos/seaMobile.mp4" : "videos/sea.mp4";
-    
     return (
-        <video className="absolute w-screen h-full object-cover" src="videos/sea.mp4" autoPlay loop muted playsInline preload="auto" onContextMenu={() => false} />
+        <video className="absolute w-screen h-full object-cover" autoPlay loop muted playsInline preload="auto" onContextMenu={() => false}>
+            <source src="videos/sea.mp4" type="video/mp4" />
+        </video>
     )
 }
 
